acc5d5d3fd864bbe0cba439687e3c50f63003432
Syeda Faryal Zehra <l242556@lhr.nu.edu.pk>
2026-04-12 17:00:12 +0500

    Update app.py

--- ServiceSathi/backend/app.py
+++ ServiceSathi/backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import pyodbc
 import os                     
 from dotenv import load_dotenv  
-
+from datetime import date, datetime
 
 load_dotenv()
 
